807ed930eb589bd92a1919c79d2d99703258e7da
Saimadan Vasamsetty <sai.vasamsetty@netenrich.com>
2023-12-18 16:16:06 +0530

    Shell

---
+++ Intro.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2 Failed $N"
+    else
+        echo -e "$G $2 Installation Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R You are not a root user $N "
+else
+    echo -e " $G You are a root user $N "
+fi
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+    else
+        echo "Already installed Skipping"
