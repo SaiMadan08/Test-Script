7a09c5b249a6f6bc4d2a6ef59999c2c31db7344b
Saimadan Vasamsetty <sai.vasamsetty@netenrich.com>
2023-12-19 19:51:32 +0530

    Shell

---
+++ deletelogs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="/tmp/shellscript-logs"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+if [ ! -d $DIR ]
+then
+    echo "$R Error File doesn't exists $N "
+fi
+
+FILES_TO_DELETE=$(find $DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r line
+do
+    echo "Deleting file $line"
+    rm -rf $line
+done <<< $FILES_TO_DELETE
