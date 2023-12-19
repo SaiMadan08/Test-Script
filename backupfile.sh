f6b0f8ab34d036b3024835af49fa8cf236e8c339
Saimadan Vasamsetty <sai.vasamsetty@netenrich.com>
2023-12-19 20:39:03 +0530

    Shell

--- backupfile.sh
+++ backupfile.sh
@@ -5,6 +5,16 @@ Y="\e[33m"
 G="\e[32m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R $2 Failed $N"
+        exit 1
+    else
+        echo -e "$G $2 Installation Success $N"
+    fi
+}
+
 SOURCE_FILE=$1
 
 DESTINATION_FILE=$2
@@ -21,6 +31,7 @@ if [ -d "$DESTINATION_FILE" ]
 then
     echo -e " $Y Destination File Located $N "
     cp -r "$SOURCE_FILE" "$DESTINATION_FILE"
+    VALIDATE $? "Backing up the file"
 else
     echo -e " $R Destination File is not detected $N"
     exit 1
