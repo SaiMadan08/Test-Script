0811b5578cc5109ac9992d55e6e072c2d880d78f
Saimadan Vasamsetty <sai.vasamsetty@netenrich.com>
2023-12-19 20:30:06 +0530

    Shell

---
+++ backupfile.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+R="\e[31m"
+Y="\e[33m"
+G="\e[32m"
+
+SOURCE_FILE=$1
+read $SOURCE_FILE
+
+DESTINATION_FILE=$2
+read $DESTINATION_FILE
+
+if [ -d "$SOURCE_FILE" ]
+then
+    echo -e " $Y Source File Located $N "
+else
+    echo -e " $R Source File is no detected $N "
+    exit 1
+fi
+
+if [ -d "$DESTINATION_FILE" ]
+then
+    echo -e " $Y Destination File Located $N "
+    cp -r "$SOURCE_FILE" "$DESTINATION_FILE"
+else
+    echo -e " $R Destination File is not detected $N"
+    exit 1
+fi
+
